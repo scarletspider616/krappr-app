0e7971f970da1eb63dce8f3fb11cb125b0b5a7df
Joey Fallone <j.fallone@ualberta.ca>
2019-12-29 14:06:25 -0700

    Use 3rd party lib for email validation

--- krappr/lib/src/utilities/validator.dart
+++ krappr/lib/src/utilities/validator.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
+import 'package:email_validator/email_validator.dart';
 
 // https://stackoverflow.com/questions/58115715/flutter-submit-login-bloc-using-stream-rxdart
 
 class Validators {
   final validateEmail = StreamTransformer<String, String>.fromHandlers(
       handleData: (String email, EventSink<String> sink) {
-    if (email.contains('@')) {
+    if (EmailValidator.validate(email)) {
       sink.add(email);
     } else {
       sink.addError('Enter a valid email');
