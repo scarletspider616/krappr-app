4396572c67697852987102de8bad01740be5372f
aaronphilips <aphilips@ualberta.ca>
2020-01-07 00:33:34 -0700

    Add stub reviews page
    Just so there was another screen to navigate to

---
+++ lib/src/ui/review_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../bloc/list_view_drawer_bloc.dart';
+
+class ReviewScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ReviewScreenState();
+}
+
+class ReviewScreenState extends State<ReviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Review Screen"),
+      ),
+      drawer: ListViewDrawerBloc().getDefaultListDrawer(),
+      body: Text("Reviews"),
+    );
+  }
+}
