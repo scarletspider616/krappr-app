991dbfa6e0bd0eecc6348d73506ba59b9caac34c
Joey Fallone <j.fallone@ualberta.ca>
2020-01-07 21:11:56 -0700

    Use retro map theme by default

--- lib/src/ui/map_widget.dart
+++ lib/src/ui/map_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import 'package:flutter/services.dart' show rootBundle;
+
 class MapWidget extends StatefulWidget {
   @override
   _MapWidgetState createState() => _MapWidgetState();
@@ -8,11 +10,24 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
   GoogleMapController mapController;
+  String _mapStyle;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
 
+  @override
+  void initState() {
+    super.initState();
+
+    rootBundle
+        .loadString('assets/style/map/default_map_theme.json')
+        .then((jsonString) {
+      _mapStyle = jsonString;
+    });
+  }
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    mapController.setMapStyle(_mapStyle);
   }
 
   @override
