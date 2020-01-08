2b17e0c8dbc63d3ce102194955f9e6dc62ca891c
Joey Fallone <j.fallone@ualberta.ca>
2020-01-07 20:49:13 -0700

    provide a default dark theme
    This dark theme is automatically activated on iOS and Android when the
    system-wide dark theme is enabled
    
    (Includes OSes that automatically switch between light/dark theme
    depending on time of day)

--- lib/src/ui/theme_data.dart
+++ lib/src/ui/theme_data.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 final ThemeData appTheme = buildDefaultTheme();
+final ThemeData darkAppTheme = buildDarkTheme();
 
 ThemeData buildDefaultTheme() {
   return (ThemeData(
+    primarySwatch: Colors.blue,
+  ));
+}
+
+ThemeData buildDarkTheme() {
+  return (ThemeData(
+    brightness: Brightness.dark,
     primarySwatch: Colors.blue,
   ));
 }
