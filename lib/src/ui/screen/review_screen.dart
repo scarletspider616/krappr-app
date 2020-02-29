0191fab9e0dbd44abb11e71f8d47426f028fb89e
John Symborski <symborsk@ualberta.ca>
2020-02-29 00:41:35 -0700

    WIP - initial review screen work

--- lib/src/ui/screen/review_screen.dart
+++ lib/src/ui/screen/review_screen.dart
@@ -12,11 +12,42 @@ class ReviewScreenState extends State<ReviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(REVIEW_SCREEN),
-      ),
-      drawer: ListViewDrawerBloc().getDefaultListDrawer(),
-      body: Text(REVIEWS),
-    );
+        appBar: AppBar(
+          title: Text("Review Screen"),
+        ),
+        drawer: ListViewDrawerBloc().getDefaultListDrawer(),
+        body: Container (
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              SizedBox(height: 10),
+              titleSummary(),
+              SizedBox(height: 10),
+              reviewStars(2),
+              SizedBox(height: 10),
+              Text("I really enjoyed the feeling of the seat on my ass.")
+            ]),
+            alignment: FractionalOffset(0.5, 0.5)
+            ));
   }
+}
+
+Widget reviewStars(int numberOfStars) {
+  return Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Icon(Icons.star, color: numberOfStars > 0 ? Colors.green[500] : Colors.black),
+      Icon(Icons.star, color: numberOfStars > 1 ? Colors.green[500] : Colors.black),
+      Icon(Icons.star, color: numberOfStars > 2 ? Colors.green[500] : Colors.black),
+      Icon(Icons.star, color: numberOfStars > 3 ? Colors.green[500] : Colors.black),
+      Icon(Icons.star, color: numberOfStars > 4 ? Colors.green[500] : Colors.black),
+    ],
+  );
+}
+
+Widget titleSummary() {
+  return Text(
+    "C-Tire Bathroom",
+    style: TextStyle(fontSize: 30.0),
+  );
 }
