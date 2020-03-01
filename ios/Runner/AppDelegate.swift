d3bf26354aa08efc38e8eb6280bad9cb05cb06d7
Joey Fallone <j.fallone@ualberta.ca>
2020-02-29 18:16:42 -0700

    Uprev version to 9.0 for flutter_auth0

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,11 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import FlutterAuth0Plugin
-
-// - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-//   return [FlutterAuth0Plugin application:app openURL:url options:options];
-// }
+import flutter_auth0
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -15,7 +11,12 @@ import FlutterAuth0Plugin
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     GMSServices.provideAPIKey(Constants.GoogleMapApiKey)
-    return FlutterAuth0Plugin.application(app, open: url, options: launchOptions)
-    // return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func application(_ app: UIApplication,
+                               open url: URL,
+                               options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FlutterAuth0Plugin.application(app, open: url, options: options)
+    }
 }
