efd15037516c754902acd18af341d4abf427642d
John Symborski <symborsk@ualberta.ca>
2020-02-29 19:21:50 -0700

    Made the rating more reusable by putting it in a bloc

--- lib/src/ui/widget/range_slider_rating.dart
+++ lib/src/ui/widget/range_slider_rating.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:krappr/src/bloc/rating_range_bloc.dart';
 
 class RangeSliderRating extends StatefulWidget {
   @override
@@ -6,24 +7,34 @@ class RangeSliderRating extends StatefulWidget {
 }
 
 class _RangeSliderRatingState extends State<RangeSliderRating> {
-  RangeValues _values = RangeValues(1, 2);
-
   @override
   Widget build(BuildContext context) {
+    RangeValues defaultValues = RangeValues(3, 5);
 
+    return StreamBuilder(
+      stream: ratingRangeBloc.ratingRangeBloc,
+      builder: (context, AsyncSnapshot<RangeValues> snapshot) {
+        if (snapshot.hasData) {
+          return buildSlider(Theme.of(context).sliderTheme, snapshot.data);
+        } else {
+          return buildSlider(Theme.of(context).sliderTheme, defaultValues);
+        }
+      },
+    );
+  }
+
+  Widget buildSlider(SliderThemeData sliderTheme, RangeValues values) {
     return Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
       SliderTheme(
         data: Theme.of(context).sliderTheme,
         child: RangeSlider(
-          values: _values,
+          values: values,
           min: 0,
           max: 5,
           divisions: 10,
-          labels: RangeLabels('${_values.start}', '${_values.end}'),
+          labels: RangeLabels('${values.start}', '${values.end}'),
           onChanged: (RangeValues values) {
-            setState(() {
-              _values = values;
-            });
+            ratingRangeBloc.updateRatingRange(values);
           },
         ),
       ),
